d51bdbe475d5aaa32c3d2fb19e592319dd85e81e
Andrea Collet <andrea.collet@accenture.com>
2020-04-19 18:04:02 +0200

    Add main logic

---
+++ index.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+
+    const userEmail = 'collet.andrea@gmail.com';
+    const userPassword = ',Accatas74accatastA,';
+
+    const login = async function () {
+        const loginInputFkey = await loginForm.$('input[name="fkey"]')[0];
+        const loginInputSsrc = await loginForm.$('input[name="ssrc"]')[0];
+        const loginInputEmail = await loginForm.$('input#email');
+        const loginInputPassword = await loginForm.$('input#password');
+        const loginSubmit = await loginForm.$('button#submit-button');
+        await page.type('input#email', userEmail, {delay: 100});
+        await page.type('input#password', userPassword, {delay: 100});
+        await Promise.all([
+            loginSubmit.click(),
+            page.waitForNavigation({waitUntil: 'networkidle2'})
+        ]).then((response) => {
+            console.log('Wverything went well');
+        }).catch((reason) => {
+            throw 'There was an error', reason;
+        });
+    }
+
+    const gotoLogin = async function () {
+        await login();
+    }
+
+    // Load StackOverflow page
+    const page = await browser.newPage();
+    await page.setViewport({width: 1440, height: 900});
+    await page.goto('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f');
+
+    // Check if page has been loaded correctly
+    if (!page) {
+        throw ('Page not loaded because error');
+    }
+
+    console.log('Page loaded correctly');
+    const loginForm = await page.$('#login-form');
+
+    // Check if login form can be found
+    if (loginForm) {
+        console.log('Login form found');
+        await login();
+    } else {
+        console.log('Login form not found. Logging in manually.')
+        await gotoLogin();
+    }
+
+    await browser.close();
+})();
