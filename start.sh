1d246f45e7717ec6f2a0b77bb482684c6175c177
Andrea Collet <collet.andrea@gmail.com>
2020-12-30 15:48:26 +0100

    Init nvm if not present

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if hash nvm 2>/dev/null; then
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    cd $PWD
+fi
+
+nvm use
 npm run start
