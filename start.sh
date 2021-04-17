d79eb66a232b27ebcead0c71dccde6d06a990ce5
Andrea Collet <collet.andrea@gmail.com>
2021-04-17 16:06:09 +0200

    Add message if nvm is not found

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 if hash nvm 2>/dev/null; then
+    echo "nvm not found!"
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     cd $PWD
