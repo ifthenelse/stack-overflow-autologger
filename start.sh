a48c9e953906cc985ef3e0aefb7650c891b29761
Andrea Collet <collet.andrea@gmail.com>
2021-04-17 16:06:35 +0200

    Check that launching script is not run as sudo

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/zsh
 
+# use scripts-common https://gitlab.com/bertrand-benoit/scripts-common
+currentDir=$( dirname "$( command -v "$0" )" )
+scriptsCommonUtilities="$currentDir/scripts-common/utilities.sh"
+[ ! -f "$scriptsCommonUtilities" ] && echo -e "ERROR: scripts-common utilities not found, you must initialize your git submodule once after you cloned the repository:\ngit submodule init\ngit submodule update" >&2 && exit 1
+# shellcheck disable=1090
+. "$scriptsCommonUtilities"
+
+if isRootUser; then
+  errorMessage "This script should not be launched with root user."
+fi
+
+
 if hash nvm 2>/dev/null; then
     echo "nvm not found!"
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
