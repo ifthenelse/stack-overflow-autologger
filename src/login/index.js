e6e5978f8597c0415919bd4784f98870285b81d6
Andrea Collet <collet.andrea@gmail.com>
2021-04-18 22:28:18 +0200

    Refactor login. Replace if .... else with try ... catch ...

--- src/login/index.js
+++ src/login/index.js
@@ -1,34 +1,31 @@
 import puppeteer from 'puppeteer-core';
 
 const login = async (page) => {
-    const loginForm = await page.$('#login-form');
+    try {
+        const loginForm = await page.$('#login-form');
+        const userEmail = process.env.USEREMAIL;
+        const userPassword = process.env.USERPASSWORD;
     
-    // Check if login form can be found
-    if (loginForm) {
-        console.log('Login form found');
-        await login(page);
-    } else {
-        console.log('Login form not found. Logging in manually.')
+        const loginInputFkey = await loginForm.$('input[name="fkey"]')[0];
+        const loginInputSsrc = await loginForm.$('input[name="ssrc"]')[0];
+        const loginInputEmail = await loginForm.$('input#email');
+        const loginInputPassword = await loginForm.$('input#password');
+        const loginSubmit = await loginForm.$('button#submit-button');
+        await page.type('input#email', userEmail, {delay: 100});
+        await page.type('input#password', userPassword, {delay: 100});
+        await Promise.all([
+            loginSubmit.click(),
+            page.waitForNavigation({waitUntil: 'networkidle2'})
+        ]).then((response) => {
+            console.log('Everything went well');
+            resolve(response);
+        }).catch((reason) => {
+            reject(reason);
+        });
     }
-
-    const userEmail = process.env.USEREMAIL;
-    const userPassword = process.env.USERPASSWORD;
-
-    const loginInputFkey = await loginForm.$('input[name="fkey"]')[0];
-    const loginInputSsrc = await loginForm.$('input[name="ssrc"]')[0];
-    const loginInputEmail = await loginForm.$('input#email');
-    const loginInputPassword = await loginForm.$('input#password');
-    const loginSubmit = await loginForm.$('button#submit-button');
-    await page.type('input#email', userEmail, {delay: 100});
-    await page.type('input#password', userPassword, {delay: 100});
-    await Promise.all([
-        loginSubmit.click(),
-        page.waitForNavigation({waitUntil: 'networkidle2'})
-    ]).then((response) => {
-        console.log('Everything went well');
-    }).catch((reason) => {
-        throw 'There was an error', reason;
-    });
+    catch (err) {
+        throw 'Couldn\'t complete login: ', err.message
+    }
 };
 
 export default login;
