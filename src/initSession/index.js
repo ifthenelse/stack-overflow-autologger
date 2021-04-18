370de3f041dbe2e90144fdf1af7c0940371d3a22
Andrea Collet <collet.andrea@gmail.com>
2021-04-18 15:40:04 +0200

    Wrap initSession inside a try...catch... statement

--- src/initSession/index.js
+++ src/initSession/index.js
@@ -1,8 +1,15 @@
 import puppeteer from 'puppeteer-core';
 
-const initSession = async () => puppeteer.launch({
-    headless: process.env.HEADLESS,
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
-});
+const initSession = async () => {
+    try {
+        return puppeteer.launch({
+            headless: process.env.HEADLESS,
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+        });
+    }
+    catch(err) {
+        throw 'Error initializing Puppeteer session: ', err.message
+    }
+}
 
 export default initSession;
