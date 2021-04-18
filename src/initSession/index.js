c1d0efcf2cd64b3142ec558147bd50a6dc1f6ebf
Andrea Collet <collet.andrea@gmail.com>
2021-04-18 14:11:13 +0200

    Remove await inside initSession

--- src/initSession/index.js
+++ src/initSession/index.js
@@ -1,10 +1,8 @@
 import puppeteer from 'puppeteer-core';
 
-const initSession = async () => {
-    return await puppeteer.launch({
-        headless: process.env.HEADLESS,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
-    });
-};
+const initSession = async () => puppeteer.launch({
+    headless: process.env.HEADLESS,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+});
 
 export default initSession;
